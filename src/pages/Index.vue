c49ea9316bcf32fb8ee8058a940315845ae319a4
ldiebold <luke@ldiebold.com>
2021-05-24 09:21:21 +1000

    setup

---
+++ src/pages/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page padding>
+    <q-select
+      v-model="selected"
+      borderless
+      :options="['Luke', 'Shannen', 'Panda', 'Lily']"
+    />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageIndex',
+
+  data () {
+    return {
+      selected: null
+    }
+  }
+})
+</script>
