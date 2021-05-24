77185995cbe49986957d97ad5ca1a8416976b63c
ldiebold <luke@ldiebold.com>
2021-05-25 09:38:54 +1000

    skeleton-and-icons

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,12 +1,12 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+  <q-layout view="hHh Lpr lFf">
+    <q-header>
       <q-toolbar>
         <q-btn
           flat
           dense
           round
-          icon="menu"
+          icon="mdi-menu"
           aria-label="Menu"
           @click="toggleLeftDrawer"
         />
@@ -15,7 +15,11 @@
           Quasar App
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <q-btn
+          flat
+          round
+          icon="mdi-logout"
+        />
       </q-toolbar>
     </q-header>
 
@@ -26,18 +30,36 @@
       class="bg-grey-1"
     >
       <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
+        <q-toolbar class="bg-primary" />
 
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
+        <q-item class="bg-grey-3">
+          <q-item-section>
+            Lists
+          </q-item-section>
+
+          <q-item-section side>
+            <q-btn
+              icon="mdi-plus"
+              size="sm"
+              flat
+              round
+            />
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable>
+          <q-item-section side>
+            <q-icon name="mdi-shopping" />
+          </q-item-section>
+
+          <q-item-section>
+            Shopping List
+          </q-item-section>
+
+          <q-item-section side>
+            12
+          </q-item-section>
+        </q-item>
       </q-list>
     </q-drawer>
 
@@ -48,52 +70,6 @@
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
 
 import { defineComponent, ref } from 'vue'
 
@@ -101,14 +77,12 @@ export default defineComponent({
   name: 'MainLayout',
 
   components: {
-    EssentialLink
   },
 
   setup () {
     const leftDrawerOpen = ref(false)
 
     return {
-      essentialLinks: linksList,
       leftDrawerOpen,
       toggleLeftDrawer () {
         leftDrawerOpen.value = !leftDrawerOpen.value
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,52 @@
 <template>
-  <q-page padding>
-    <q-select
-      v-model="selected"
-      borderless
-      :options="['Luke', 'Shannen', 'Panda', 'Lily']"
-    />
+  <q-page class="row">
+    <q-card class="col-xs-9">
+      <q-toolbar>
+        <q-input
+          dense
+          placeholder="Search"
+          borderless
+          class="full-width"
+          :model-value="search"
+        >
+          <template #append>
+            <q-icon name="mdi-magnify" />
+          </template>
+        </q-input>
+      </q-toolbar>
+      <q-list bordered>
+        <q-item>
+          <q-item-section side>
+            <q-checkbox :model-value="true" />
+          </q-item-section>
+
+          <q-item-section>
+            Go Shopping
+          </q-item-section>
+
+          <q-item-section side>
+            <q-btn
+              flat
+              round
+              size="sm"
+              icon="mdi-delete"
+            />
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-card>
+
+    <div class="col-xs-3">
+      <q-toolbar class="bg-primary">
+        <q-btn
+          fab
+          style="margin-bottom: -42px;"
+          class="q-ml-lg"
+          color="secondary"
+          icon="mdi-plus"
+        />
+      </q-toolbar>
+    </div>
   </q-page>
 </template>
 
@@ -16,7 +58,7 @@ export default defineComponent({
 
   data () {
     return {
-      selected: null
+      search: ''
     }
   }
 })
