765ebf3fdd347bf25a4d33f319b6f8f1713bec70
ldiebold <luke@ldiebold.com>
2021-05-25 19:38:03 +1000

    Pages, Layouts and Routing

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,49 @@
+<template>
+  <q-page
+    class="flex flex-center"
+    padding
+  >
+    <div
+      class="full-width"
+      style="max-width: 300px;"
+    >
+      <h5 class="q-my-md text-center">
+        Login
+      </h5>
+      <q-card>
+        <q-card-section>
+          <q-form>
+            <q-input
+              filled
+              label="email"
+              placeholder="email@exmaple.com"
+              class="q-mb-md"
+            />
+            <q-input
+              filled
+              label="password"
+              type="password"
+              class="q-mb-sm"
+            />
+            <q-checkbox
+              label="remember me"
+              :model-value="true"
+            />
+          </q-form>
+        </q-card-section>
+
+        <q-btn
+          label="login"
+          class="full-width"
+          color="primary"
+        />
+      </q-card>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  // name: 'PageName',
+}
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,20 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      {
+        path: '',
+        component: () => import('pages/Index.vue')
+      }
+    ]
+  },
+  {
+    path: '/auth',
+    component: () => import('layouts/BlankLayout.vue'),
+    children: [
+      {
+        path: 'login',
+        component: () => import('pages/LoginPage.vue')
+      }
     ]
   },
 
