fb8d6b5740c2421d93811ca6d3f45f3556cb33eb
ldiebold <luke@ldiebold.com>
2021-06-26 16:21:31 +1000

    Layout - Call To Action

---
+++ src/components/CallToAction.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="row justify-center text-white bg-primary q-py-xl q-px-lg">
+    <h4
+      style="max-width: 450px;"
+      class="q-my-none"
+      :class="screen.gt.xs ? 'text-left' : 'text-center'"
+    >
+      A fully featured ToDo app you'll fall in love with ❤️
+    </h4>
+
+    <div class="q-pa-md">
+      <q-btn
+        color="secondary"
+        label="try it now"
+        size="lg"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { useQuasar } from 'quasar'
+
+export default {
+  // name: 'ComponentName',
+  setup () {
+    const $q = useQuasar()
+    return {
+      screen: $q.screen
+    }
+  }
+}
+</script>
--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -2,22 +2,50 @@
   <q-page>
     <!-- Feature -->
     <div
+      v-if="false"
+      style="min-height: 95vh"
       class="flex flex-center"
     >
       <Feature
-        class="justify-center"
-        style="max-width: 900px;"
+        class="justify-center full-width"
       />
+    </div>
+
+    <div
+      v-if="false"
+      style="min-height: 95vh"
+      class="flex flex-center bg-grey-2"
+    >
+      <Feature
+        class="justify-center full-width"
+      />
+    </div>
+
+    <div
+      v-if="false"
+      style="min-height: 95vh"
+      class="flex flex-center"
+    >
+      <Feature
+        class="justify-center full-width"
+      />
+    </div>
+
+    <!-- Call To Action -->
+    <div v-if="true">
+      <CallToAction />
     </div>
   </q-page>
 </template>
 
 <script>
 import Feature from 'components/Feature.vue'
+import CallToAction from 'components/CallToAction.vue'
 
 export default {
   components: {
-    Feature
+    Feature,
+    CallToAction
   }
 }
 </script>
